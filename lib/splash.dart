f8672a647eb554cd707ebfe6c57b9bab97dddc13
kamal taleb <kamal.talebb@gmail.com>
2024-05-13 03:39:32 +0300

    senior flutter

---
+++ lib/splash.dart
@@ -0,0 +1,40 @@
+import 'package:beauty_center/welcomeScreen.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: 'Welcome Screen',
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Center(
+          child: Container(
+            child: Image(
+              image: AssetImage('images/logo.png'),
+            ),
+          ),
+        ));
+  }
+}
