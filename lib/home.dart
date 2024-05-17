14a4f8ebd23e7759569e6a4779522e65891aa4a0
kamal taleb <kamal.talebb@gmail.com>
2024-05-17 17:17:32 +0300

    appbar

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,13 @@
 import 'package:beauty_center/appbar.dart';
+import 'package:beauty_center/cart_menu_icon.dart';
 import 'package:beauty_center/curved_edge.dart';
 import 'package:beauty_center/curved_edge_widget.dart';
+import 'package:beauty_center/home_appbar.dart';
 import 'package:beauty_center/primary_header_container.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
 import 'circular_container.dart';
@@ -12,37 +17,14 @@ class homePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SPrimaryHeaderContainer(
               child: Column(
-                children: [
-                  SAppbar(
-                    title: Column(
-                      children: [
-                        Text(
-                          "hello",
-                        ),
-                        Text(
-                          "sanyar",
-                        ),
-                      ],
-                    ),
-                    actions: [
-                      Stack(
-                        children: [
-                          // IconButton(
-                          //   onPressed: () {},
-                          //   icon: Icon(Iconsax.shopping_bag),
-                          // ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
+                children: [SHomeAppBar()],
               ),
             ),
           ],
--- lib/home_appbar.dart
+++ lib/home_appbar.dart
@@ -1,10 +1,31 @@
+import 'package:beauty_center/appbar.dart';
+import 'package:beauty_center/cart_menu_icon.dart';
 import 'package:flutter/material.dart';
 
 class SHomeAppBar extends StatelessWidget {
-  const SHomeAppBar({super.key});
+  const SHomeAppBar({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SAppbar(
+      title: Column(
+        children: [
+          Text(
+            "hello",
+          ),
+          Text(
+            "sanyar",
+          ),
+        ],
+      ),
+      actions: [
+        SCartCounterIcon(
+          onPressed: () {},
+          iconColor: Colors.white,
+        )
+      ],
+    );
   }
 }
