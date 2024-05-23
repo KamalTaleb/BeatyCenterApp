cea539753009d018248e23daafbbd400df3e5b38
Hassanadlouni <104458103+Hassanadlouni@users.noreply.github.com>
2024-05-23 21:26:20 +0300

    appointment

---
+++ lib/appointment_page.dart
@@ -0,0 +1,113 @@
+import 'package:beauty_center/enums.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SAppointmentPage extends StatefulWidget {
+  const SAppointmentPage({super.key});
+
+  @override
+  State<SAppointmentPage> createState() => _SAppointmentPageState();
+}
+
+class _SAppointmentPageState extends State<SAppointmentPage> {
+  FilterStatus status = FilterStatus.upcomming;
+  Alignment _alignment = Alignment.centerLeft;
+
+  List<dynamic> schedules = [
+    {
+      "staff_name":"staff 1",
+      "staff_profile":"images/home1.JPG",
+      "category":"hair dye",
+      "status":FilterStatus.upcomming,
+    },
+    {
+      "staff_name":"staff 2",
+      "staff_profile":"images/home1.JPG",
+      "category":"hair dye",
+      "status":FilterStatus.complete,
+    },
+    {
+      "staff_name":"staff 3",
+      "staff_profile":"images/home1.JPG",
+      "category":"hair dye",
+      "status":FilterStatus.complete,
+    },
+    {
+      "staff_name":"staff 4",
+      "staff_profile":"images/home1.JPG",
+      "category":"hair dye",
+      "status":FilterStatus.cancel,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<dynamic> filteredSchedules = schedules.where((var schedule){
+      // switch(schedule['status']){
+      //   case 'upcomming':
+      //     schedule['status'] = FilterStatus.upcomming;
+      //     break;
+      //   case 'complete':
+      //     schedule['status'] = FilterStatus.complete;
+      //     break;
+      //   case 'cancel':
+      //     schedule['status'] = FilterStatus.cancel;
+      //     break;
+      // }
+      return schedule ['status'] == status;
+    }).toList();
+
+    return SafeArea(
+        child: Padding(
+      padding: EdgeInsets.only(left: 20.0,top: 20.0,right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget> [
+              const Text(
+                'Appointment Schedule',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // Config.spaceSmall,
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for(FilterStatus filterStatus in FilterStatus.values)
+                          Expanded(
+                            child: GestureDetector(
+                            onTap: (){
+                              if(filterStatus == FilterStatus.upcomming){
+                                status = FilterStatus.upcomming;
+                                _alignment = Alignment.centerLeft;
+                              }else if(filterStatus == FilterStatus.complete){
+                                status = FilterStatus.upcomming;
+                                _alignment = Alignment.center;
+                              }else if(filterStatus == FilterStatus.cancel){
+                                status = FilterStatus.upcomming;
+                                _alignment = Alignment.centerRight;
+                              }
+                            },
+                          ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+    ));
+  }
+}
--- lib/enums.dart
+++ lib/enums.dart
@@ -2,3 +2,4 @@
 enum TextSizes{small, medium, large}
 enum OrderStatus{processing, shipped, delivered}
 enum PaymentMethods{paypal, googlePay, applePay, visa, masterCard, paystack, razorPay, paytm}
+enum FilterStatus{upcomming, complete, cancel}
